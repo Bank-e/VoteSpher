2452244ecfa076cc3e1fb529896ac309450735b5
Piyachat Salangsing <145454535+Bank-e@users.noreply.github.com>
2026-04-18 21:06:31 +0700

    Revert "Feat/verify otp request"

--- internal/models/admin.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Admin struct {
-    ID      uint  `gorm:"primaryKey;autoIncrement;column:admin_id"` // ใช้ ID แต่ผูกกับคอลัมน์ admin_id
-    VoterID uint  `gorm:"not null;uniqueIndex"`
-    Voter   Voter // ไม่ต้องใส่ Tag เลย GORM ผูกให้อัตโนมัติ!
-}
